417ed539d3750c1c1c8e908d01747672f813704b
naninani-hub <70853852+naninani-hub@users.noreply.github.com>
2021-12-02 11:52:46 +0530

    Add files via upload

---
+++ Addtwonumbers.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author ItLab
+ */
+public class mavenproject1 {  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
